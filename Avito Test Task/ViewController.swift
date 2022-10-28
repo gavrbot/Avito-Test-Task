d1f258926f4efc4c06bb58354af54c2e2310e11e
gavrbot <asakurazik2@gmail.com>
2022-10-28 11:59:31 +0400

    Initial Commit

---
+++ Avito Test Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Avito Test Task
+//
+//  Created by Александр Гаврилов on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
